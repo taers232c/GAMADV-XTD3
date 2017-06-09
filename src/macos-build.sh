e5103e0f47f91109009125dfa558cd8c3ac204fc
Ross Scroggs <ross.scroggs@gmail.com>
2017-06-09 10:59:35 -0700

    Error handling cleanup.

--- src/macos-build.sh
+++ src/macos-build.sh
@@ -3,7 +3,7 @@ rm -rf build
 rm -rf dist
 rm -rf gamadv-x3-$1-macos.tar.xz
 
-/Library/Frameworks/Python.framework/Versions/2.7/bin/pyinstaller --clean -F --distpath=gam macos-gam.spec
+/Library/Frameworks/Python.framework/Versions/3.6/bin/pyinstaller --clean -F --distpath=gam macos-gam.spec
 cp LICENSE gam
 cp license.rtf gam
 cp whatsnew.txt gam
