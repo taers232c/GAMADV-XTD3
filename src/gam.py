600d70e20233c2a4446ac9355c99329c1616af92
Ross Scroggs <ross.scroggs@gmail.com>
2017-08-28 06:33:53 -0700

    Correct typo

--- src/gam.py
+++ src/gam.py
@@ -23,7 +23,7 @@ For more information, see https://github.com/taers232c/GAMADV-X
 """
 
 __author__ = u'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = u'4.53.16
+__version__ = u'4.53.16'
 __license__ = u'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import sys
