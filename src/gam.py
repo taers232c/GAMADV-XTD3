f6bd34fd30540f3113f9f0a7c386cd4eb10d9471
Ross Scroggs <ross.scroggs@gmail.com>
2018-04-17 09:40:15 -0700

    Increment version

--- src/gam.py
+++ src/gam.py
@@ -22,7 +22,7 @@ For more information, see https://github.com/taers232c/GAMADV-XTD
 """
 
 __author__ = u'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = u'4.55.59'
+__version__ = u'4.56.00'
 __license__ = u'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import sys
