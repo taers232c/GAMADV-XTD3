72d5d15902ced895fad0c2c665bffa394dc245a6
Ross Scroggs <ross.scroggs@gmail.com>
2018-08-01 16:49:19 -0700

    Update scopes to reflect Google's deprecation of https://www.googleapis.com/auth/plus.login.

--- src/gam.py
+++ src/gam.py
@@ -22,7 +22,7 @@ For more information, see https://github.com/taers232c/GAMADV-XTD
 """
 
 __author__ = u'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = u'4.57.24'
+__version__ = u'4.57.25'
 __license__ = u'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import sys
