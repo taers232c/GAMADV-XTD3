f3d1d489e0e03c3decdd72a0c636ddd3646ef07d
Ross Scroggs <ross.scroggs@gmail.com>
2019-06-07 06:00:56 -0700

    Fixed bug in `gam report <ActivityApplicationName>` that caused a trap.

--- src/gam.py
+++ src/gam.py
@@ -22,7 +22,7 @@ For more information, see https://github.com/taers232c/GAMADV-XTD3
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '4.86.00'
+__version__ = '4.86.01'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import base64
@@ -7800,7 +7800,7 @@ def doReport():
                 else:
                   purge_parameters = False
               if purge_parameters:
-                event.pop('parameters')
+                event.pop('parameters', None)
               row = flattenJSON(event, flattened={})
               row.update(activity_row)
               csvPF.WriteRowTitles(row)
