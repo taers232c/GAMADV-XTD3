ac7e067424d7e031eac334edb34a4491356cf919
Ross Scroggs <ross.scroggs@gmail.com>
2019-07-25 09:37:52 -0700

    Update linux-x86_64-install.sh

--- src/travis/linux-x86_64-install.sh
+++ src/travis/linux-x86_64-install.sh
@@ -5,17 +5,17 @@ if [ "$VMTYPE" == "test" ]; then
 else
   $python -OO -m PyInstaller --clean --noupx --strip -F --distpath=gamadv-xtd3 $GAMOS-gam.spec
   export gam="gamadv-xtd3/gam"
-  export gampath=$(readlink -e gamadv-xtd3)
+#  export gampath=$(readlink -e gamadv-xtd3)
+  export gampath=gamadv-xtd3
   export GAMVERSION=`$gam version simple`
-  cp LICENSE $gampath
-  cp license.rtf $gampath
-  cp Gam*.txt $gampath
-  cp cacerts.pem $gampath
+  cp LICENSE $gampath/
+  cp license.rtf $gampath/
+  cp Gam*.txt $gampath/
+  cp cacerts.pem $gampath/
   this_glibc_ver=$(ldd --version | awk '/ldd/{print $NF}')
   GAM_ARCHIVE=gam-$GAMVERSION-$GAMOS-$PLATFORM-glibc$this_glibc_ver.tar.xz
-  rm $gampath/lastupdatecheck.txt
-  tar cfJ $GAM_ARCHIVE gamadv-xtd3/
+  tar cfJ $GAM_ARCHIVE $gampath/
   echo "PyInstaller GAM info:"
-  du -h gamadv-xtd3/gam
+  du -h $gampath/gam
   time $gam version extended
 fi
