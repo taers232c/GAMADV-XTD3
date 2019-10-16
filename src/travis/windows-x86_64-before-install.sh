c4bee1c51018df1f9cb36cd64dca68311aad14c2
Ross Scroggs <ross.scroggs@gmail.com>
2019-10-16 06:20:41 -0700

    Travis updates

--- src/travis/windows-x86_64-before-install.sh
+++ src/travis/windows-x86_64-before-install.sh
@@ -1,31 +1,22 @@
 echo "Installing Net-Framework-Core..."
 export mypath=$(pwd)
 until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
-cd ~
-#export exefile=Win64OpenSSL_Light-${BUILD_OPENSSL_VERSION//./_}.exe
-#if [ ! -e $exefile ]; then
-#  echo "Downloading $exefile..."
-#  wget --quiet https://slproweb.com/download/$exefile
-#fi
-#echo "Installing $exefile..."
-#powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"
-#cinst -y python3
-PYVER=$(~/python/python.exe -V)
-PYRESULT=$?
-if [[ "$PYRESULT" != "0" ]] || [[ "$PYVER" != *"$BUILD_PYTHON_VERSION"* ]]; then
-  rm -rf python
-  mkdir python
-  echo "Downloading Python $BUILD_PYTHON_VERSION..."
-  wget --quiet https://www.python.org/ftp/python/$BUILD_PYTHON_VERSION/python-$BUILD_PYTHON_VERSION-embed-amd64.zip
-  7z e python-$BUILD_PYTHON_VERSION-embed-amd64.zip -oC:\Users\travis\python
+cd ~/pybuild
+export exefile=Win64OpenSSL_Light-${BUILD_OPENSSL_VERSION//./_}.exe
+if [ ! -e $exefile ]; then
+  echo "Downloading $exefile..."
+  wget --quiet https://slproweb.com/download/$exefile
 fi
+echo "Installing $exefile..."
+powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"
+cinst -y python3
 until cinst -y wixtoolset; do echo "trying again..."; done
-#until cp -v /c/ssl/libcrypto-1_1-x64.dll /c/Python37/DLLs/libcrypto-1_1.dll; do echo "trying again..."; done
-#until cp -v /c/ssl/libssl-1_1-x64.dll /c/Python37/DLLs/libssl-1_1.dll; do echo "trying again..."; done
-export PATH=$PATH:/c/Users/travis/python/scripts
+until cp -v /c/ssl/libcrypto-1_1-x64.dll /c/Python37/DLLs/libcrypto-1_1.dll; do echo "trying again..."; done
+until cp -v /c/ssl/libssl-1_1-x64.dll /c/Python37/DLLs/libssl-1_1.dll; do echo "trying again..."; done
+export PATH=$PATH:/c/Python37/scripts
 cd $mypath
-export python=/c/Users/travis/python/python.exe
-export pip=/c/Users/travis/python/scripts/pip.exe
+export python=/c/Python37/python.exe
+export pip=pip
 
 $pip install --upgrade pip
 $pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 $pip install -U
@@ -36,12 +27,9 @@ $pip install --upgrade -r src/requirements.txt
 # lots of malware uses PyInstaller default bootloader
 # https://stackoverflow.com/questions/53584395/how-to-recompile-the-bootloader-of-pyinstaller
 echo "Downloading PyInstaller..."
-#wget --quiet https://github.com/pyinstaller/pyinstaller/releases/download/v$PYINSTALLER_VERSION/PyInstaller-$PYINSTALLER_VERSION.tar.gz
-wget --quiet https://github.com/pyinstaller/pyinstaller/archive/develop.tar.gz
-#tar xf PyInstaller-$PYINSTALLER_VERSION.tar.gz
-tar xf develop.tar.gz
-#cd PyInstaller-$PYINSTALLER_VERSION/bootloader
-cd pyinstaller-develop/bootloader
+wget --quiet https://github.com/pyinstaller/pyinstaller/releases/download/v$PYINSTALLER_VERSION/PyInstaller-$PYINSTALLER_VERSION.tar.gz
+tar xf PyInstaller-$PYINSTALLER_VERSION.tar.gz
+cd PyInstaller-$PYINSTALLER_VERSION/bootloader
 echo "bootloader before:"
 md5sum ../PyInstaller/bootloader/Windows-64bit/*
 $python ./waf all --target-arch=64bit
