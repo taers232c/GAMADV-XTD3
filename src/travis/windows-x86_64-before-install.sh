abc7d8a541020b7cddad15d35627b31e716c04c0
Ross Scroggs <ross.scroggs@gmail.com>
2020-02-01 11:10:21 -0800

    Try installing lxml and crptyography with pip

--- src/travis/windows-x86_64-before-install.sh
+++ src/travis/windows-x86_64-before-install.sh
@@ -20,10 +20,9 @@ export pip=/c/Python38/scripts/pip.exe
 
 $pip install --upgrade pip
 $pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 $pip install -U
-$pip install --upgrade lxml==4.5.0
 #$pip install src/lxml-4.5.0-cp38-cp38-win_amd64.whl
-$pip install src/cryptography-2.8-cp38-cp38-win_amd64.whl
-#$pip install --upgrade -r src/requirements.txt
+#$pip install src/cryptography-2.8-cp38-cp38-win_amd64.whl
+$pip install --upgrade -r src/requirements.txt
 
 #$pip install --upgrade pyinstaller
 # Install PyInstaller from source and build bootloader
