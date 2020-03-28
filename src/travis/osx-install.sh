640db39f0c5a4564f41a6588cde3c496045eaa01
Ross Scroggs <ross.scroggs@gmail.com>
2020-03-27 17:27:59 -0700

    Travis updates from Jay

---
+++ src/travis/osx-install.sh
@@ -0,0 +1,15 @@
+cd src
+echo "MacOS Version Info According to Python:"
+python -c "import platform; print(platform.mac_ver())"
+$python -OO -m PyInstaller --clean --noupx --strip -F --distpath=gam $GAMOS-gam.spec
+export gam="gam/gam"
+export gampath=gam
+$gam version extended
+export GAMVERSION=`gam/gam version simple`
+cp LICENSE gam
+cp whatsnew.txt gam
+cp GamCommands.txt gam
+MACOSVERSION=$(defaults read loginwindow SystemVersionStampAsString)
+GAM_ARCHIVE=gam-$GAMVERSION-$GAMOS-$PLATFORM-MacOS$MACOSVERSION.tar.xz
+rm gam/lastupdatecheck.txt
+tar cfJ $GAM_ARCHIVE gam/
