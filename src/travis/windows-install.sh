015e5cbbe8a71fce20a87649c45797287bda9144
Ross Scroggs <ross.scroggs@gmail.com>
2020-06-18 09:22:50 -0700

    Windows build cleanup

--- src/travis/windows-install.sh
+++ src/travis/windows-install.sh
@@ -4,10 +4,6 @@ export gampath="dist/gamadv-xtd3"
 rm -rf $gampath
 mkdir -p $gampath
 export gampath=$(readlink -e $gampath)
-echo "Where's pyinstaller?"
-whereis pyinstaller
-which pyinstaller
-pyinstaller --version
 pyinstaller --clean --noupx -F --distpath $gampath gam.spec
 export gam="${gampath}/gam"
 echo "running compiled GAM..."
