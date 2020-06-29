47c0910be296a74a8de73cea139abcde1e607580
Ross Scroggs <ross.scroggs@gmail.com>
2020-06-28 18:01:04 -0700

    Travis updates

--- src/travis/linux-install.sh
+++ src/travis/linux-install.sh
@@ -1,38 +1,38 @@
 cd src
 if [[ "$TRAVIS_JOB_NAME" == *"Testing" ]]; then
-  export gam="$python gam.py"
+  export gam="$python -m gam"
   export gampath=$(readlink -e .)
 else
-  export gampath="gamadv-xtd3"
+  export gampath="dist/gamadv-xtd3"
   rm -rf $gampath
-  mkdir $gampath
+  mkdir -p $gampath
+  export gampath=$(readlink -e $gampath)
   $python -OO -m PyInstaller --clean --noupx --strip -F --distpath $gampath gam.spec
-  export gam="$gampath/gam"
+  export gam="${gampath}/gam"
   export GAMVERSION=`$gam version simple | head -n 1 | cut -c1-7`
   cp LICENSE $gampath/
   cp license.rtf $gampath/
   cp Gam*.txt $gampath/
   cp cacerts.pem $gampath/
   this_glibc_ver=$(ldd --version | awk '/ldd/{print $NF}')
-  GAM_ARCHIVE=$gampath-$GAMVERSION-$GAMOS-$PLATFORM-glibc$this_glibc_ver.tar.xz
-  tar --create --file $GAM_ARCHIVE --xz $gampath/
+  GAM_ARCHIVE=gamadv-xtd3-$GAMVERSION-$GAMOS-$PLATFORM-glibc$this_glibc_ver.tar.xz
+  # tar will cd to dist and tar up gam/
+  tar -C dist/ --create --file $GAM_ARCHIVE --xz gamadv-xtd3
   echo "PyInstaller GAM info:"
-  du -h $gampath/gam
+  du -h $gam
   time $gam version extended
-
   if [ "${TRAVIS_DIST}" == "precise" ] && [ "${PLATFORM}" == "x86_64" ]; then
-    GAM_LEGACY_ARCHIVE=$gampath-${GAMVERSION}-${GAMOS}-${PLATFORM}-legacy.tar.xz
-#    $python -OO -m staticx $gampath/gam $gampath/gam-staticx
-    $python -OO -m staticx -l /lib/x86_64-linux-gnu/libresolv.so.2 -l /lib/x86_64-linux-gnu/libnss_dns.so.2 $gampath/gam $gampath/gam-staticx
-    strip $gampath/gam-staticx
+    GAM_LEGACY_ARCHIVE=gamadv-xtd3-${GAMVERSION}-${GAMOS}-${PLATFORM}-legacy.tar.xz
+    $python -OO -m staticx -l /lib/x86_64-linux-gnu/libresolv.so.2 -l /lib/x86_64-linux-gnu/libnss_dns.so.2 $gam $gam-staticx
+    strip $gam-staticx
     rm $gampath/gam
-    mv $gampath/gam-staticx $gampath/gam
-    chmod 755 $gampath/gam
-    tar --create --file $GAM_LEGACY_ARCHIVE --xz $gampath/
+    mv $gam-staticx $gam
+    chmod 755 $gam
+    tar -C dist/ --create --file $GAM_LEGACY_ARCHIVE --xz gamadv-xtd3
     echo "Legacy StaticX GAM info:"
-    du -h $gampath/gam
+    du -h $gam
     time $gam version extended
   fi
   echo "GAM packages:"
-  ls -l $gampath-*.xz
+  ls -l gamadv-xtd3-*.tar.xz
 fi
--- src/travis/osx-install.sh
+++ src/travis/osx-install.sh
@@ -1,9 +1,9 @@
 cd src
 echo "MacOS Version Info According to Python:"
 python -c "import platform; print(platform.mac_ver())"
-export gampath="gamadv-xtd3"
+export gampath="dist/gamadv-xtd3"
 rm -rf $gampath
-mkdir $gampath
+mkdir -p $gampath
 $python -OO -m PyInstaller --clean --noupx --strip -F --distpath $gampath gam.spec
 export gam="$gampath/gam"
 $gam version extended
@@ -13,6 +13,7 @@ cp Gam*.txt $gampath/
 cp LICENSE $gampath/
 cp cacerts.pem $gampath/
 MACOSVERSION=$(defaults read loginwindow SystemVersionStampAsString | cut -c1-5)
-GAM_ARCHIVE=$gampath-$GAMVERSION-$GAMOS-$MACOSVERSION-$PLATFORM.tar
+GAM_ARCHIVE=gamadv-xtd3-$GAMVERSION-$GAMOS-$MACOSVERSION-$PLATFORM.tar
 echo "GAM Archive:" $GAM_ARCHIVE
-tar --create --file $GAM_ARCHIVE $gampath/
+# tar will cd to dist/ and tar up gam/
+tar -C dist/ --create --file $GAM_ARCHIVE gamadv-xtd3
