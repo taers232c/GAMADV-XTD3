39ed6ed959bef416cbe63540eacc20483fdcb7d7
Ross Scroggs <ross.scroggs@gmail.com>
2021-05-14 08:16:13 -0700

    Update decrypt.sh

--- .github/actions/decrypt.sh
+++ .github/actions/decrypt.sh
@@ -12,7 +12,7 @@ fi
 
 gpg --quiet --batch --yes --decrypt --passphrase="${PASSCODE}" --output "${credsfile}" "${gpgfile}"
 
--mkdir -p ${gampath}/ConfigGitHub
+mkdir -p ${gampath}/ConfigGitHub
 tar xvvf "${credsfile}" --directory "${gampath}"
 echo "${gampath}/ConfigGitHub files:"
 ls -l ${gampath}/ConfigGitHub
