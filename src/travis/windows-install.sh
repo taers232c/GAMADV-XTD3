1bcda5a38a62b4850270855163c7499b04d18c2a
Ross Scroggs <ross.scroggs@gmail.com>
2021-05-18 09:22:34 -0700

    Rebuild pyinstaller bootloader - 5

--- src/travis/windows-install.sh
+++ src/travis/windows-install.sh
@@ -4,6 +4,7 @@ export gampath="dist/gamadv-xtd3"
 rm -rf $gampath
 mkdir -p $gampath
 export gampath=$(readlink -e $gampath)
+echo "Compile: $python -OO -m pyinstaller/PyInstaller --clean --noupx -F --distpath $gampath gam.spec"
 $python -OO -m pyinstaller/PyInstaller --clean --noupx -F --distpath $gampath gam.spec
 export gam="${gampath}/gam"
 echo "running compiled GAM..."
