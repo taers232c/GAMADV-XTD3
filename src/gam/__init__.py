58e2eacb6a3ebe641d0e56bdc2e44d225068a1f2
Ross Scroggs <ross.scroggs@gmail.com>
2021-10-22 11:23:50 -0700

    Updated to Python 3.10.0

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -23,7 +23,7 @@ For more information, see https://github.com/taers232c/GAMADV-XTD3
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '6.08.27'
+__version__ = '6.09.00'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import base64
