cbda0256187daee3964ac1ad31978981f52cbd8c
Ross Scroggs <ross.scroggs@gmail.com>
2022-01-19 21:02:49 -0800

    More lxml MacOS hacking

--- src/travis/macos-before-install.sh
+++ src/travis/macos-before-install.sh
@@ -1,18 +1,35 @@
 mypath=$(pwd)
 cpucount=$(sysctl -n hw.ncpu)
+export LD_LIBRARY_PATH=~/ssl/lib
 echo "This device has $cpucount CPUs for compiling..."
 
-brew install coreutils
-brew install bash
-
-# prefer standard GNU tools like date over MacOS defaults
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$(brew --prefix)/opt/gnu-tar/libexec/gnubin:$PATH"
-
-date --version
-gdate --version
-bash --version
-
-cd ~
+SSLVER=$(~/ssl/bin/openssl version)
+SSLRESULT=$?
+if [ $SSLRESULT -ne 0 ] || [[ "$SSLVER" != "OpenSSL $MACOS_BUILD_OPENSSL_VERSION "* ]]; then
+  echo "SSL Result: $SSLRESULT - SSL Ver: $SSLVER"
+  if [ $SSLRESULT -ne 0 ]; then
+    echo "sslresult -ne 0"
+  fi
+  if [[ "$SSLVER" != "OpenSSL $MACOS_BUILD_OPENSSL_VERSION "* ]]; then
+    echo "sslver not equal to..."
+  fi
+  cd ~
+  rm -rf ssl
+  mkdir ssl
+# Compile latest OpenSSL # wget --quiet https://www.openssl.org/source/openssl-$MACOS_BUILD_OPENSSL_VERSION.tar.gz
+  ls -l ${mypath}/sslinstalls
+  cp ${mypath}/sslinstalls/openssl-$MACOS_BUILD_OPENSSL_VERSION.tar.gz .
+  echo "Extracting OpenSSL..."
+  tar xf openssl-$MACOS_BUILD_OPENSSL_VERSION.tar.gz
+  cd openssl-$MACOS_BUILD_OPENSSL_VERSION
+  echo "Compiling OpenSSL $MACOS_BUILD_OPENSSL_VERSION..."
+  ./config shared --prefix=$HOME/ssl
+  echo "Running make for OpenSSL..."
+  make -j$cpucount -s
+  echo "Running make install for OpenSSL..."
+  make install > /dev/null
+  cd ~
+fi
 
 # Use official Python.org version of Python which is backwards compatible
 # with older MacOS versions
@@ -23,19 +40,10 @@ wget https://www.python.org/ftp/python/$BUILD_PYTHON_VERSION/$pyfile
 echo "Installing Python $BUILD_PYTHON_VERSION..."
 sudo installer -pkg ./$pyfile -target /
 
-# This fixes https://github.com/pyinstaller/pyinstaller/issues/5062
-#codesign --remove-signature /Library/Frameworks/Python.framework/Versions/3.9/Python
-
-brew install openssl@3
-
 cd ~
 
 export python=/usr/local/bin/python3
 export pip=/usr/local/bin/pip3
-SSLVER=$($openssl version)
-SSLRESULT=$?
-PYVER=$($python -V)
-PYRESULT=$?
 
 $python -V
 
@@ -46,4 +54,4 @@ $pip install --upgrade pip
 $pip install --upgrade packaging
 $pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 $pip install -U
 $pip install --upgrade -r src/requirements.txt
-$pip install --upgrade git+git://github.com/pyinstaller/pyinstaller.git@$PYINSTALLER_VERSION
+$pip install --upgrade git+https://github.com/pyinstaller/pyinstaller.git@$PYINSTALLER_VERSION
