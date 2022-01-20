f92ac5a75fa0cf71594bb008ebb56f43560be535
Ross Scroggs <ross.scroggs@gmail.com>
2022-01-19 16:24:47 -0800

    Revert to previous version of macos-before-install.sh

--- src/travis/macos-before-install.sh
+++ src/travis/macos-before-install.sh
@@ -1,36 +1,18 @@
 mypath=$(pwd)
 cpucount=$(sysctl -n hw.ncpu)
-echo "We are running on Ubuntu $TRAVIS_DIST $PLATFORM"
-export LD_LIBRARY_PATH=~/ssl/lib
 echo "This device has $cpucount CPUs for compiling..."
 
-SSLVER=$(~/ssl/bin/openssl version)
-SSLRESULT=$?
-if [ $SSLRESULT -ne 0 ] || [[ "$SSLVER" != "OpenSSL $LINUX_BUILD_OPENSSL_VERSION "* ]]; then
-  echo "SSL Result: $SSLRESULT - SSL Ver: $SSLVER"
-  if [ $SSLRESULT -ne 0 ]; then
-    echo "sslresult -ne 0"
-  fi
-  if [[ "$SSLVER" != "OpenSSL $LINUX_BUILD_OPENSSL_VERSION "* ]]; then
-    echo "sslver not equal to..."
-  fi
-  cd ~
-  rm -rf ssl
-  mkdir ssl
-# Compile latest OpenSSL # wget --quiet https://www.openssl.org/source/openssl-$LINUX_BUILD_OPENSSL_VERSION.tar.gz
-  ls -l ${mypath}/sslinstalls
-  cp ${mypath}/sslinstalls/openssl-$LINUX_BUILD_OPENSSL_VERSION.tar.gz .
-  echo "Extracting OpenSSL..."
-  tar xf openssl-$LINUX_BUILD_OPENSSL_VERSION.tar.gz
-  cd openssl-$LINUX_BUILD_OPENSSL_VERSION
-  echo "Compiling OpenSSL $LINUX_BUILD_OPENSSL_VERSION..."
-  ./config shared --prefix=$HOME/ssl
-  echo "Running make for OpenSSL..."
-  make -j$cpucount -s
-  echo "Running make install for OpenSSL..."
-  make install > /dev/null
-  cd ~
-fi
+brew install coreutils
+brew install bash
+
+# prefer standard GNU tools like date over MacOS defaults
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$(brew --prefix)/opt/gnu-tar/libexec/gnubin:$PATH"
+
+date --version
+gdate --version
+bash --version
+
+cd ~
 
 # Use official Python.org version of Python which is backwards compatible
 # with older MacOS versions
@@ -41,10 +23,19 @@ wget https://www.python.org/ftp/python/$BUILD_PYTHON_VERSION/$pyfile
 echo "Installing Python $BUILD_PYTHON_VERSION..."
 sudo installer -pkg ./$pyfile -target /
 
+# This fixes https://github.com/pyinstaller/pyinstaller/issues/5062
+#codesign --remove-signature /Library/Frameworks/Python.framework/Versions/3.9/Python
+
+brew install openssl@3
+
 cd ~
 
 export python=/usr/local/bin/python3
 export pip=/usr/local/bin/pip3
+SSLVER=$($openssl version)
+SSLRESULT=$?
+PYVER=$($python -V)
+PYRESULT=$?
 
 $python -V
 
@@ -55,4 +46,4 @@ $pip install --upgrade pip
 $pip install --upgrade packaging
 $pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 $pip install -U
 $pip install --upgrade -r src/requirements.txt
-$pip install --upgrade git+https://github.com/pyinstaller/pyinstaller.git@$PYINSTALLER_VERSION
+$pip install --upgrade git+git://github.com/pyinstaller/pyinstaller.git@$PYINSTALLER_VERSION
