ca857a4647a59602229aba94c6b5e3ea19a5807f
Ross Scroggs <ross.scroggs@gmail.com>
2022-03-07 10:54:28 -0800

    OpenSSL 3.0.1 install fix

--- src/travis/windows-before-install.sh
+++ src/travis/windows-before-install.sh
@@ -1,12 +1,10 @@
 if [[ "$PLATFORM" == "x86_64" ]]; then
   export BITS="64"
   export PYTHONFILE_BITS="-amd64"
-  export OPENSSL_BITS="-x64"
   export WIX_BITS="x64"
 elif [[ "$PLATFORM" == "x86" ]]; then
   export BITS="32"
   export PYTHONFILE_BITS=""
-  export OPENSSL_BITS=""
   export WIX_BITS="x86"
 fi
 echo "This is a ${BITS}-bit build for ${PLATFORM}"
@@ -56,12 +54,15 @@ export exefile=Win${BITS}OpenSSL_Light-${WINDOWS_BUILD_OPENSSL_VERSION//./_}.exe
 ls -l ${mypath}/sslinstalls
 cp ${mypath}/sslinstalls/${exefile} .
 until powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"; do echo "trying openssl again..."; sleep 3; done
-until cp -v /c/ssl/libcrypto-${WINDOWS_LIB_SSL_VERSION}${OPENSSL_BITS}.dll /c/python/DLLs/; do echo "trying libcrypto copy again..."; sleep 3; done
-until cp -v /c/ssl/libssl-${WINDOWS_LIB_SSL_VERSION}${OPENSSL_BITS}.dll /c/python/DLLs/; do echo "trying libssl copy again..."; done
 if [[ "$PLATFORM" == "x86_64" ]]; then
-  cp -v /c/python/DLLs/libssl-${WINDOWS_LIB_SSL_VERSION}-x64.dll /c/python/DLLs/libssl-${WINDOWS_LIB_SSL_VERSION}.dll
-  cp -v /c/python/DLLs/libcrypto-${WINDOWS_LIB_SSL_VERSION}-x64.dll /c/python/DLLs/libcrypto-${WINDOWS_LIB_SSL_VERSION}.dll
+  until cp -v /c/python/DLLs/libssl-${WINDOWS_LIB_SSL_VERSION}-x64.dll /c/python/DLLs/libssl-${WINDOWS_LIB_SSL_VERSION}.dll; do echo "trying libcrypto copy again..."; sleep 3; done
+  until cp -v /c/python/DLLs/libcrypto-${WINDOWS_LIB_SSL_VERSION}-x64.dll /c/python/DLLs/libcrypto-${WINDOWS_LIB_SSL_VERSION}.dll; do echo "trying libssl copy again..."; done
+else
+  until cp -v /c/ssl/libcrypto-${WINDOWS_LIB_SSL_VERSION}.dll /c/python/DLLs/; do echo "trying libcrypto copy again..."; sleep 3; done
+  until cp -v /c/ssl/libssl-${WINDOWS_LIB_SSL_VERSION}.dll /c/python/DLLs/; do echo "trying libssl copy again..."; done
 fi
+ls -l /c/python/DLLs/libcrypto*
+ls -l /c/python/DLLs/libssl*
 
 cd $mypath
 
