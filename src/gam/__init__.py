173e6eb25556447e3487d9d713a4c00a85ad7cc3
Ross Scroggs <ross.scroggs@gmail.com>
2022-05-26 10:21:42 -0700

    Fix typos

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '6.22.15'
+__version__ = '6.22.16'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
@@ -54794,8 +54794,8 @@ SPREADSHEET_FIELDS_CHOICE_MAP = {
   'namedranges': 'namedRanges',
   'properties': 'properties',
   'sheets': 'sheets',
-  'spreadcheetid': 'spreadsheetId',
-  'spreadcheeturl': 'spreadsheetUrl',
+  'spreadsheetid': 'spreadsheetId',
+  'spreadsheeturl': 'spreadsheetUrl',
   }
 
 # gam <UserTypeEntity> info|show sheet <DriveFileEntity>
