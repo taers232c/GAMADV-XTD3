7ffb3aeffcfa1ea705c32c8bc7f28c471f60574f
Ross Scroggs <ross.scroggs@gmail.com>
2022-08-30 19:05:42 -0700

    Add Linux Jammy

--- src/gam-install.sh
+++ src/gam-install.sh
@@ -30,8 +30,8 @@ upgrade_only=false
 gamversion="latest"
 adminuser=""
 regularuser=""
-gam_x86_64_glibc_vers="2.31 2.27 2.23 2.19 2.15"
-gam_arm64_glibc_vers="2.31 2.27 2.23"
+gam_x86_64_glibc_vers="2.35 2.31 2.27 2.23 2.19 2.15"
+gam_arm64_glibc_vers="2.35 2.31 2.27 2.23"
 strip_gamadv_xtd3="--strip-components 0"
 
 while getopts "hd:a:o:b:lp:u:r:v:s" OPTION
