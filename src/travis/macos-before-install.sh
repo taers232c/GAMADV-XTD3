bed1598f0dcccbafb2471d884841d3927c6468b0
Ross Scroggs <ross.scroggs@gmail.com>
2022-09-10 08:17:24 -0700

    x86_64 Mac build cleanup

--- src/travis/macos-before-install.sh
+++ src/travis/macos-before-install.sh
@@ -3,34 +3,6 @@ cpucount=$(sysctl -n hw.ncpu)
 export LD_LIBRARY_PATH=~/ssl/lib
 echo "This device has $cpucount CPUs for compiling..."
 
-#SSLVER=$(~/ssl/bin/openssl version)
-#SSLRESULT=$?
-#if [ $SSLRESULT -ne 0 ] || [[ "$SSLVER" != "OpenSSL $MACOS_BUILD_OPENSSL_VERSION "* ]]; then
-#  echo "SSL Result: $SSLRESULT - SSL Ver: $SSLVER"
-#  if [ $SSLRESULT -ne 0 ]; then
-#    echo "sslresult -ne 0"
-#  fi
-#  if [[ "$SSLVER" != "OpenSSL $MACOS_BUILD_OPENSSL_VERSION "* ]]; then
-#    echo "sslver not equal to..."
-#  fi
-#  cd ~
-#  rm -rf ssl
-#  mkdir ssl
-## Compile latest OpenSSL # wget --quiet https://www.openssl.org/source/openssl-$MACOS_BUILD_OPENSSL_VERSION.tar.gz
-#  ls -l ${mypath}/sslinstalls
-#  cp ${mypath}/sslinstalls/openssl-$MACOS_BUILD_OPENSSL_VERSION.tar.gz .
-#  echo "Extracting OpenSSL..."
-#  tar xf openssl-$MACOS_BUILD_OPENSSL_VERSION.tar.gz
-#  cd openssl-$MACOS_BUILD_OPENSSL_VERSION
-#  echo "Compiling OpenSSL $MACOS_BUILD_OPENSSL_VERSION..."
-#  ./config shared --prefix=$HOME/ssl
-#  echo "Running make for OpenSSL..."
-#  make -j$cpucount -s
-#  echo "Running make install for OpenSSL..."
-#  make install > /dev/null
-#  cd ~
-#fi
-
 # Use official Python.org version of Python which is backwards compatible
 # with older MacOS versions
 export pyfile=python-$BUILD_PYTHON_VERSION-macos11.pkg
