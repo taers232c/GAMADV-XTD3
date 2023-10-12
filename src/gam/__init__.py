b0330c7e65d79df4422139217354ac77d054983e
Ross Scroggs <ross.scroggs@gmail.com>
2023-10-12 09:51:35 -0700

    Bump version

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '6.64.12'
+__version__ = '6.64.13'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
