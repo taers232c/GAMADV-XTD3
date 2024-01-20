084851b5d0bff6b97deb23ee6ded95b7c356f1bc
Ross Scroggs <ross.scroggs@gmail.com>
2024-01-19 19:32:39 -0800

    Use local copy of `googleapiclient` to remove static discovery documents to improve performance.

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '6.67.22'
+__version__ = '6.67.23'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
@@ -108,10 +108,10 @@ from filelock import FileLock
 
 from pathvalidate import sanitize_filename, sanitize_filepath
 
-import googleapiclient
-import googleapiclient.discovery
-import googleapiclient.errors
-import googleapiclient.http
+#import googleapiclient
+#import googleapiclient.discovery
+#import googleapiclient.errors
+#import googleapiclient.http
 import google.oauth2.credentials
 import google.oauth2.id_token
 import google.auth
@@ -150,6 +150,10 @@ import gdata.apps.contacts
 import gdata.apps.contacts.service
 import gdata.apps.sites
 import gdata.apps.sites.service
+import googleapiclient
+import googleapiclient.discovery
+import googleapiclient.errors
+import googleapiclient.http
 from iso8601 import iso8601
 
 IS08601_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
