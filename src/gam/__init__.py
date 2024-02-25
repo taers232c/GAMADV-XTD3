7b8db6433b2b7d736a19643596da1deed645a56e
Ross Scroggs <ross.scroggs@gmail.com>
2024-02-25 10:23:43 -0800

    Updated `gam info users <UserTypeEntity>` to not include group tree infornation unless option `grouptree` is specified.

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '6.70.05'
+__version__ = '6.70.06'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
@@ -42228,7 +42228,8 @@ def infoUsers(entityList):
 
   cd = buildGAPIObject(API.DIRECTORY)
   ci = None
-  getAliases = getBuildingNames = getCIGroupsTree = getGroups = getGroupsTree = getLicenses = getSchemas = not GC.Values[GC.QUICK_INFO_USER]
+  getAliases = getBuildingNames = getCIGroupsTree = getGroups = getLicenses = getSchemas = not GC.Values[GC.QUICK_INFO_USER]
+  getGroupsTree = False
   FJQC = FormatJSONQuoteChar()
   projection = 'full'
   customFieldMask = None
