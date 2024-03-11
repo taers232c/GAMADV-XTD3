c77d761aafd9f1baf3bcfbed679608fec8ede1f9
Ross Scroggs <ross.scroggs@gmail.com>
2024-03-10 21:06:25 -0700

    Revert "Updated to pyinstaller 6.5.0."
    This reverts commit b15a189348c43aa7600327762a092b03a7feb0a1.

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '6.72.00'
+__version__ = '6.71.13'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
