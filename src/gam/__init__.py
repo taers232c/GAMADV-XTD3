05b0c967650cddb71a190091a99ad368ad667365
Ross Scroggs <ross.scroggs@gmail.com>
2024-09-13 11:26:17 -0700

    Updated `gam update group postmaster@domain.com` to handle the error that is generated.

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '6.81.01'
+__version__ = '6.81.02'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
