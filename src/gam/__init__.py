d0bf86fc376744d396b585a32279395edebf9f66
Ross Scroggs <ross.scroggs@gmail.com>
2024-10-01 15:15:47 -0700

    Updated to Python 3.12.7 where possible.

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '7.00.10'
+__version__ = '7.00.11'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
