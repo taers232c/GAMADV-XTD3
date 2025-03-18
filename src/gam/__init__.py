9f464aa1479c4ff5cd00d419bcbd0119a50e2539
Ross Scroggs <ross.scroggs@gmail.com>
2025-03-18 10:21:24 -0700

    Increment version

--- src/gam/__init__.py
+++ src/gam/__init__.py
@@ -25,7 +25,7 @@ https://github.com/taers232c/GAMADV-XTD3/wiki
 """
 
 __author__ = 'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = '7.05.09'
+__version__ = '7.05.10'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 #pylint: disable=wrong-import-position
